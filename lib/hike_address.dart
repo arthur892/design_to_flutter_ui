3068399064ea62a4ec762971aac0ab2d5d59964a
arthur Windows <rybalkin.arthur@gmail.com>
2024-09-26 10:42:00 +0200

    changed button

--- lib/hike_address.dart
+++ lib/hike_address.dart
@@ -1,6 +1,53 @@
 import 'package:design_to_flutter_ui/hike_name_and_address.dart';
 import 'package:flutter/material.dart';
 
+class HikeAddress extends StatefulWidget {
+  const HikeAddress({super.key});
+
+  @override
+  State<HikeAddress> createState() {
+    // TODO: implement createState
+    return HikeAddressState();
+  }
+}
+
+class HikeAddressState extends State<HikeAddress> {
+  Icon myIcon = Icon(Icons.star, color: Colors.red);
+  int counter = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const HikeNameAndAddress(),
+        const Spacer(),
+        IconButton(
+          icon: myIcon,
+          onPressed: () {
+            setState(() {
+              myIcon = Icon(Icons.thumb_up);
+              counter++;
+            });
+          },
+        ),
+        const SizedBox(width: 4),
+        Text("$counter"),
+      ],
+    );
+  }
+}
+
+class changeStar extends State<HikeAddress> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+
+/*
 class HikeAddress extends StatelessWidget {
   const HikeAddress({
     super.key,
@@ -8,15 +55,21 @@ class HikeAddress extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        HikeNameAndAddress(),
-        Spacer(),
-        Icon(Icons.star, color: Colors.red),
-        SizedBox(width: 4),
-        Text("41"),
+        const HikeNameAndAddress(),
+        const Spacer(),
+        IconButton(
+          icon: const Icon(Icons.star, color: Colors.red),
+          onPressed: () {
+            print("Button gedrückt");
+          },
+        ),
+        const SizedBox(width: 4),
+        const Text("41"),
       ],
     );
   }
 }
+*/
